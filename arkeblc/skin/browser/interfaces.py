2417f4b65a57b10046f9abf8d43c1923a664af6c
Alain Meurant <alain.meurant@affinitic.be>
2010-02-05 20:48:55 +0000

    remise en ordre de l'egg ##

--- arkeblc/skin/browser/interfaces.py
+++ arkeblc/skin/browser/interfaces.py
@@ -1,5 +1,6 @@
 from plone.theme.interfaces import IDefaultPloneLayer
 
-class IThemeSpecific(IDefaultPloneLayer):
-    """Marker interface that defines a Zope 3 browser layer.
+class IArkeblcTheme(IDefaultPloneLayer):
+    """
+    Thme fo cercle archeologique brain-le-comte
     """
